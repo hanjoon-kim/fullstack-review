0e3e14c12b4e1f4a4eeae9a0d476412896c5dac0
hanjoon-kim <hanjoon3@gmail.com>
2018-07-13 12:11:20 -0700

    Implement mongoose schema and search react component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -6,6 +6,7 @@ class Search extends React.Component {
     this.state = {
       term: ''
     }
+    this.search = this.search.bind(this);
   }
 
   onChange (e) {
@@ -21,7 +22,7 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.terms} onChange={(e) => this.onChange(e)}/>       
       <button onClick={this.search}> Add Repos </button>
     </div>) 
   }
--- database/index.js
+++ database/index.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+})
+
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  name: String,
+  url: String,
+  startgazers_count: Number
+  
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+
+let save = (name, url, stargazers) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
+  Repo.create({name: name, url: url, startgazers_count: startgazers})
 }
 
 module.exports.save = save;
